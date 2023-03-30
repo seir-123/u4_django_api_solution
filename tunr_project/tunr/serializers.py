a7ce847c498a2417de1665774c51843c50b78f31
Jeremy <Taubman33@gmail.com>
2023-03-30 11:53:57 -0400

    serializers and cors addedd

--- tunr_project/tunr/serializers.py
+++ tunr_project/tunr/serializers.py
@@ -1,19 +1,6 @@
 from rest_framework import serializers
 from .models import Artist, Song
 
-class ArtistSerializer(serializers.HyperlinkedModelSerializer):
-    songs = serializers.HyperlinkedRelatedField(
-        view_name ='song_detail',
-        many=True,
-        read_only=True
-    )
-
-    class Meta:
-        model = Artist
-        fields = ('id', 'photo_url', 'nationality', 'name', 'songs')
-
-
-
 
 class SongSerializer(serializers.HyperlinkedModelSerializer):
     artist = serializers.HyperlinkedRelatedField(
@@ -21,6 +8,30 @@ class SongSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True
     )
 
+    artist_id = serializers.PrimaryKeyRelatedField(
+        queryset=Artist.objects.all(),
+        source='artist'
+    )
+
     class Meta:
         model = Song
-        fields = ('id', 'artist', 'title', 'album', 'preview_url')
+        fields = ('id' , 'artist_id', 'artist', 'title', 'album', 'preview_url')
+
+
+class ArtistSerializer(serializers.HyperlinkedModelSerializer):
+    songs = SongSerializer(
+        many = True,
+        read_only =True
+    )
+
+    artist_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='artist_detail'
+    )
+
+    class Meta:
+        model = Artist
+        fields = ('id', 'artist_url','photo_url', 'nationality', 'name', 'songs')
+
+
+
+
